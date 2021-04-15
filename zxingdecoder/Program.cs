d77a416f042a09a23789643da07ac9114fe475fc
mft <52249075+byturkcanoglu@users.noreply.github.com>
2021-04-15 15:42:30 +0300

    first commit

---
+++ zxingdecoder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ZXing;
+using System.Drawing;
+using System.IO;
+
+namespace zxingdecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"/Users/mfturkcanoglu/Downloads/mini-foto";
+
+            var files = System.IO.Directory.GetFiles(path);
+
+            QRReader reader = QRReader.getInstance(files[0]);
+
+            int count = 0;
+            for (int i= 0;i<files.Length;i++)
+            {
+
+                Console.WriteLine($"Path : {files[i]}");
+
+
+                reader.ImagePath = $@"{files[i]}";
+               
+
+                Console.Write($"#: {i}");
+                var result = reader.ReadBarcode();
+                if (result != null) count += 1;
+                
+                Console.WriteLine("********");
+            }
+
+            Console.WriteLine($"Truths Count : {count}");
+        }
+    }
+}
---
+++ zxingdecoder/QRReader.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using ZXing;
+using System.IO;
+using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using ZXing.Multi.QrCode;
+using ZXing.Common;
+
+namespace zxingdecoder
+{
+    public class QRReader
+    {
+        private string imagePath;
+
+        public string ImagePath{
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+
+        private static QRReader qrReader = null;
+
+
+        public static QRReader getInstance(string image)
+        {
+
+            if (qrReader == null)
+            {
+                qrReader = new QRReader(image);
+            }
+
+            return qrReader;
+
+        }
+
+        public dynamic ReadBarcode()
+        {
+            try
+            {
+                var qrCodeBitMap = (Bitmap)System.Drawing.Image.FromFile(imagePath);
+
+                
+                var barcodeReader = new BarcodeReader();
+                barcodeReader.Options.TryHarder = true;
+                barcodeReader.AutoRotate = true;
+                barcodeReader.TryInverted = true;
+                
+
+                var qrcoderesult = barcodeReader.Decode(qrCodeBitMap);
+                
+                
+                if (qrcoderesult != null)
+                {
+                    Console.WriteLine($"\tText : {qrcoderesult.Text ?? "Null"}");
+                    return qrcoderesult.Text;
+                }
+                else
+                {
+                    barcodeReader.Options.ReturnCodabarStartEnd = true;
+                    SixLabors.ImageSharp.Image image = SixLabors.ImageSharp.Image.Load(imagePath);
+                    image.Mutate(img => img.Resize(image.Width * 3, image.Height * 2)
+                         .GaussianSharpen(sigma: 4.0f)
+                         .Grayscale());
+                    
+                    qrCodeBitMap = ToBitmap(image);
+                    qrcoderesult = barcodeReader.Decode(qrCodeBitMap);
+
+                    Console.WriteLine($"\tText : {qrcoderesult.Text ?? "Null"}");
+                   
+
+                    MultiFormatReader multiFormatReader = new MultiFormatReader();
+                    
+
+                    ZXing.BinaryBitmap multiBitmap;
+
+                    return qrcoderesult!=null?
+                    qrcoderesult.Text:
+                    
+                    multiBitmap = new BinaryBitmap(new HybridBinarizer(new BitmapLuminanceSource(qrCodeBitMap)));
+                    qrcoderesult = multiFormatReader.decode(multiBitmap);
+                    Console.WriteLine($"\t Multiple part of Text : {qrcoderesult.Text ?? "Null"}");
+                    return qrcoderesult;
+                }
+
+         
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("File not found!");
+                
+            }
+            catch(NullReferenceException nullBarCode)
+            {
+                Console.WriteLine("Barcode is not readable or null!");
+                
+            }
+            return null;
+        }
+
+
+        private QRReader(string image)
+        {
+            imagePath = image;
+        }
+
+
+        public static System.Drawing.Bitmap ToBitmap(SixLabors.ImageSharp.Image image) 
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var imageEncoder = image.GetConfiguration().ImageFormatsManager.FindEncoder(PngFormat.Instance);
+                image.Save(memoryStream, imageEncoder);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return new System.Drawing.Bitmap(memoryStream);
+            }
+        }
+
+
+    }
+  
+
+
+}
+
+/*
+                    ZXing.Result[] results = { };
+
+                    if(qrcoderesult == null)
+                    {
+                        results = barcodeReader.DecodeMultiple(qrCodeBitMap);
+
+                        if (results != null)
+                        {
+                            foreach (var result in results)
+                            {
+                                Console.WriteLine($"\tText : {result.Text ?? "Null"}");
+                            }
+                            return results;
+                        }
+                        return null;
+
+                    }
+
+
+ */
+
